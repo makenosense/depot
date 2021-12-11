9cb139f73b1878993a96c2201e50c3c7cf40a2ab
Nathan <nathan1108@163.com>
2021-12-11 16:38:05 +0800

    version 1.1.0

--- src/main/java/depot/MainApp.java
+++ src/main/java/depot/MainApp.java
@@ -32,7 +32,7 @@ import java.util.Objects;
 
 public class MainApp extends Application {
     public static final String APP_NAME = "Depot";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
     public static URL LOGO_URL = Objects.requireNonNull(MainApp.class.getResource("view/html/img/logo.png"));
 
     private Stage primaryStage;
